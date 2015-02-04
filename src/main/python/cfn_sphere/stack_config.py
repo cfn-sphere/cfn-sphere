41b140aea5d5655d215a2cfa7e97cefca6f19bf5
Marco Hoyer <marco@hoyer.pm>
2015-02-04 09:52:15 +0100

    first steps

---
+++ src/main/python/cfn_sphere/stack_config.py
@@ -0,0 +1,38 @@
+__author__ = 'mhoyer'
+
+from yaml.scanner import ScannerError
+import yaml
+import logging
+import os
+
+
+class NoConfigException(Exception):
+    pass
+
+
+class StackConfig(object):
+    def __init__(self, path):
+        logging.basicConfig(format='%(asctime)s %(levelname)s %(module)s: %(message)s',
+                            datefmt='%d.%m.%Y %H:%M:%S',
+                            level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+        self.path = path
+
+    def get(self):
+        try:
+            with open(self.path, 'r') as file:
+                return yaml.load(file)["stacks"]
+        except IOError as e:
+            self.logger.error("Could not read yaml file: {0}".format(e))
+            raise NoConfigException
+        except KeyError as e:
+            self.logger.error("Yaml file does not contain stacks as first key")
+            raise NoConfigException
+        except ScannerError as e:
+            self.logger.error("Could not parse yaml {0}: {1}".format(e.problem_mark, e.problem))
+            raise NoConfigException
+
+
+if __name__ == "__main__":
+    reader = StackConfig("resources/stacks.yml")
+    print reader.get()
--- src/main/python/cfn_sphere/stack_handler.py
+++ src/main/python/cfn_sphere/stack_handler.py
@@ -1,1 +1,19 @@
 __author__ = 'mhoyer'
+
+from cfn_sphere.stack_config import StackConfig
+
+
+class StackHandler(object):
+
+    def __init__(self):
+        self.stacks = StackConfig("resources/stacks.yml").get()
+
+    def sync(self):
+        for name, data in self.stacks.iteritems():
+            print name
+            print data
+
+
+if __name__ == "__main__":
+    stack_handler = StackHandler()
+    stack_handler.sync()
