c27696dd3ab3c2536be811c1f42eab7c5057b43d
Marco Hoyer <marco@hoyer.pm>
2015-02-06 11:56:16 +0100

    bla

--- src/main/python/cfn_sphere/connector/cloudformation.py
+++ src/main/python/cfn_sphere/connector/cloudformation.py
@@ -2,6 +2,7 @@ __author__ = 'mhoyer'
 
 from boto import cloudformation
 from boto.resultset import ResultSet
+from boto.exception import AWSConnectionError
 import logging
 
 
@@ -14,6 +15,10 @@ class CloudFormation(object):
         self.logger = logging.getLogger(__name__)
 
         self.conn = cloudformation.connect_to_region(region)
+        if not self.conn:
+            self.logger.error("Could not connect to cloudformation API in {0}. Invalid region?".format(region))
+            raise AWSConnectionError("Got None connection object")
+
         self.logger.info("Connected to cloudformation API at {0} with access key id: {1}".format(
             region, self.conn.aws_access_key_id))
 
@@ -29,6 +34,12 @@ class CloudFormation(object):
 
     def get_stacks(self):
         return self.stacks
+
+    def get_stacks_dict(self):
+        stacks_dict = {}
+        for stack in self.stacks:
+            stacks_dict[stack.stack_name] = {"parameters": stack.parameters, "outputs": stack.outputs}
+        return stacks_dict
 
     def create_stack(self, template):
         self.conn.create_stack("bla", template_body={}, parameters=[])
--- src/main/python/cfn_sphere/stack_handler.py
+++ src/main/python/cfn_sphere/stack_handler.py
@@ -2,40 +2,50 @@ __author__ = 'mhoyer'
 
 from cfn_sphere.stack_config import StackConfig
 from cfn_sphere.artifact_resolver import ArtifactResolver
+from cfn_sphere.connector.cloudformation import CloudFormation
+from boto.cloudformation.stack import Stack
 import logging
 
 
 class StackHandler(object):
 
-    def __init__(self, region="eu-west-1", stacks= None):
+    def __init__(self):
         logging.basicConfig(format='%(asctime)s %(levelname)s %(module)s: %(message)s',
                             datefmt='%d.%m.%Y %H:%M:%S',
                             level=logging.INFO)
         self.logger = logging.getLogger(__name__)
-
-        self.stacks = StackConfig("resources/stacks.yml").get()
-        self.artifacts = ArtifactResolver()
+        self.desired_stacks = StackConfig("resources/stacks.yml").get()
 
     def sync(self):
-        for name, data in self.stacks.iteritems():
+        for name, data in self.desired_stacks.iteritems():
             self.logger.info("### Working on stack {0} ###".format(name))
-            parameters = data.get("parameters", {})
-            for key, value in parameters.iteritems():
-                if isinstance(value, list):
-                    self.logger.info("List parameter found for {0}".format(key))
-                    for item in value:
-                        #TODO: handle value here
-                        pass
-                elif isinstance(value, basestring):
-                    self.logger.info("String parameter found for {0}".format(key))
+            existing_stacks = CloudFormation(region=data["region"]).get_stacks_dict()
+            artifacts_resolver = ArtifactResolver(region=data["region"])
+
+            if name not in existing_stacks:
+                self.logger.info("Stack <{0}> doesn't exist, will create it".format(name))
+            else:
+                self.logger.info("Stack <{0}> exists and probably needs an update".format(name))
+                #TODO: check if stack needs update
+
+            self.get_new_stack_parameters(data.get("parameters", {}))
+
+    def get_new_stack_parameters(self, parameters):
+        for key, value in parameters.iteritems():
+            if isinstance(value, list):
+                self.logger.info("List parameter found for {0}".format(key))
+                for item in value:
                     #TODO: handle value here
-                    if value.startswith("Ref::"):
-                        self.logger.info("Resolved artifact value: ".format(self.artifacts.get_artifact_value(value)))
+                    pass
+            elif isinstance(value, basestring):
+                self.logger.info("String parameter found for {0}".format(key))
+                #TODO: handle value here
+                if value.startswith("Ref::"):
+                    self.logger.info("Resolved artifact value: ".format(self.artifacts_resolver.get_artifact_value(value)))
 
                     #print "Parameter: " + key + ": " + str(value)
-                else:
-                    raise NotImplementedError("Cannot handle {0} value for key: {1}".format(type(value), key))
-
+            else:
+                raise NotImplementedError("Cannot handle {0} value for key: {1}".format(type(value), key))
 
 
 if __name__ == "__main__":
