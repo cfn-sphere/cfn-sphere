a7106009835275463bc122b3b80fcdce4fb0f487
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2015-07-31 01:26:47 +0200

    convert utility

--- src/main/python/cfn_sphere/util.py
+++ src/main/python/cfn_sphere/util.py
@@ -1,5 +1,7 @@
 
 import logging
+import json
+import yaml
 
 
 def get_logger():
@@ -7,3 +9,11 @@ def get_logger():
                         datefmt='%d.%m.%Y %H:%M:%S',
                         level=logging.INFO)
     return logging.getLogger(__name__)
+
+
+def convert_json_to_yaml(data):
+    return yaml.dump(json.loads(data))
+
+
+def convert_yaml_to_json(data):
+    return json.dumps(yaml.parse(data))
