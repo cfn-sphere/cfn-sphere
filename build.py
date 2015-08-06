00bf2cb9de69a45c1d8ca8226097c767e50676b3
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2015-08-06 23:37:17 +0200

    add artifact metadata

--- build.py
+++ build.py
@@ -1,4 +1,4 @@
-from pybuilder.core import use_plugin, init
+from pybuilder.core import use_plugin, init, Author
 
 use_plugin("python.core")
 use_plugin("python.unittest")
@@ -8,9 +8,17 @@ use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
 
-
 name = "cfn-sphere"
-default_task = "publish"
+
+authors = [Author('Marco Hoyer', 'marco_hoyer@gmx.de')]
+description = "cfn-sphere - A CLI tool intended to simplify AWS CloudFormation handling."
+license = 'APACHE LICENSE, VERSION 2.0'
+summary = 'cfn-sphere AWS CloudFormation management cli'
+url = 'https://github.com/marco-hoyer/cfn-sphere'
+version = '0.1'
+
+default_task = ['analyze', 'publish']
+
 
 @init
 def set_properties(project):
@@ -24,9 +32,21 @@ def set_properties(project):
     project.set_property('coverage_break_build', False)
     project.set_property('install_dependencies_upgrade', True)
 
+    project.set_property('distutils_classifiers', [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: APACHE LICENSE, VERSION 2.0',
+        'Programming Language :: Python',
+        'Topic :: System :: Systems Administration'
+    ])
+
+
 @init(environments='teamcity')
 def set_properties_for_teamcity_builds(project):
     import os
+
     project.set_property('teamcity_output', True)
     project.version = '%s-%s' % (project.version, os.environ.get('BUILD_NUMBER', 0))
     project.default_task = ['clean', 'install_build_dependencies', 'publish']
