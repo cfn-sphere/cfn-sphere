1beaa87fc6e881dd1468bec530b5348badb40b6a
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2015-08-20 01:38:51 +0200

    add pypi upload

--- build_and_upload_packages.sh
+++ build_and_upload_packages.sh
@@ -22,22 +22,23 @@ fpm \
   --python-install-bin '/usr/bin' --no-python-dependencies \
   --depends 'python>=2.7' --depends python-boto --depends python-click --depends python-networkx --depends python-ordereddict --depends python-yaml -s python -t rpm setup.py
 
-package_cloud push marco-hoyer/cfn-sphere/scientific/6 *.rpm
-package_cloud push marco-hoyer/cfn-sphere/debian/wheezy *.deb
-package_cloud push marco-hoyer/cfn-sphere/debian/jessie *.deb
-
 #pypi
-if [ -z "$PYPI_USER" ] && [ -z "$PYPI_PASSWORD" ]; then
-    # prepare pypi credentials config
-    cat >~/.pypirc <<EOL
-    [distutils]
-    index-servers =
-        pypi
+if [ -n "$PYPI_USER" ] && [ -n "$PYPI_PASSWORD" ]; then
+    echo "Generating pypi auth config"
+    cat > ~/.pypirc << EOF
+[distutils]
+index-servers =
+    pypi
 
-    [pypi]
-    username:$PYPI_USER
-    password:$PYPI_PASSWORD
-    EOL
+[pypi]
+username:$PYPI_USER
+password:$PYPI_PASSWORD
+EOF
 fi
 
 python setup.py sdist upload || exit 0
+
+# upload
+package_cloud push marco-hoyer/cfn-sphere/scientific/6 *.rpm
+package_cloud push marco-hoyer/cfn-sphere/debian/wheezy *.deb
+package_cloud push marco-hoyer/cfn-sphere/debian/jessie *.deb
