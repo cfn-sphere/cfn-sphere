2120577e8a063f80c9b2c8c6312030d74ae66035
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2015-08-20 01:19:18 +0200

    add pypi upload

--- build_and_upload_packages.sh
+++ build_and_upload_packages.sh
@@ -8,12 +8,14 @@ pyb
 
 cd target/dist/* &&
 
+# deb
 fpm \
   --iteration $CIRCLE_BUILD_NUM \
   --python-install-lib '/usr/lib/python2.7/site-packages' \
   --python-install-bin '/usr/bin' --no-python-dependencies \
   --depends 'python>=2.7' --depends python-boto --depends python-click --depends python-networkx --depends python-ordereddict --depends python-yaml -s python -t deb setup.py
 
+# rpm
 fpm \
   --iteration $CIRCLE_BUILD_NUM \
   --python-install-lib '/usr/lib/python2.7/dist-packages' \
@@ -23,3 +25,19 @@ fpm \
 package_cloud push marco-hoyer/cfn-sphere/scientific/6 *.rpm
 package_cloud push marco-hoyer/cfn-sphere/debian/wheezy *.deb
 package_cloud push marco-hoyer/cfn-sphere/debian/jessie *.deb
+
+#pypi
+if [ -z "$PYPI_USER" ] && [ -z "$PYPI_PASSWORD" ]; then
+    # prepare pypi credentials config
+    cat >~/.pypirc <<EOL
+    [distutils]
+    index-servers =
+        pypi
+
+    [pypi]
+    username:$PYPI_USER
+    password:$PYPI_PASSWORD
+    EOL
+fi
+
+python setup.py sdist upload || exit 0
