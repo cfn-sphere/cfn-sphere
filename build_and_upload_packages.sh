d81eccbdf52efd8e960e99a96cfb44199bc6f60d
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2015-08-21 15:25:46 +0200

    only build packages directly for specific versions, no iterations

--- build_and_upload_packages.sh
+++ build_and_upload_packages.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 set -e
 
-# set build number to 0 if not run on circle ci
-export CIRCLE_BUILD_NUM=${CIRCLE_BUILD_NUM:-0}
-
 pyb
 
 cd target/dist/* &&
 
 # deb
 fpm \
-  --iteration $CIRCLE_BUILD_NUM \
   --no-python-fix-name \
   --python-install-lib '/usr/lib/python2.7/dist-packages' \
   --python-install-bin '/usr/bin' --no-python-dependencies \
@@ -18,13 +14,18 @@ fpm \
 
 # rpm
 fpm \
-  --iteration $CIRCLE_BUILD_NUM \
   --no-python-fix-name \
   --python-install-lib '/usr/lib/python2.6/site-packages' \
   --python-install-bin '/usr/bin' --no-python-dependencies \
   --depends python --depends python-boto --depends python-click --depends python-networkx --depends python-ordereddict --depends python-yaml -s python -t rpm setup.py
 
-#pypi
+# upload deb and rpm
+package_cloud push marco-hoyer/cfn-sphere/scientific/6 *.rpm
+package_cloud push marco-hoyer/cfn-sphere/debian/wheezy *.deb
+package_cloud push marco-hoyer/cfn-sphere/debian/jessie *.deb
+package_cloud push marco-hoyer/cfn-sphere/ubuntu/trusty *.deb
+
+# upload package to pypi
 if [ -n "$PYPI_USER" ] && [ -n "$PYPI_PASSWORD" ]; then
     echo "Generating pypi auth config"
     cat > ~/.pypirc << EOF
@@ -38,10 +39,4 @@ password:$PYPI_PASSWORD
 EOF
 fi
 
-python setup.py sdist upload || exit 0
-
-# upload
-package_cloud push marco-hoyer/cfn-sphere/scientific/6 *.rpm
-package_cloud push marco-hoyer/cfn-sphere/debian/wheezy *.deb
-package_cloud push marco-hoyer/cfn-sphere/debian/jessie *.deb
-package_cloud push marco-hoyer/cfn-sphere/ubuntu/trusty *.deb
+python setup.py sdist upload
