d5b0f888ac5a7f6b77a061c6409d48091bb21f19
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2015-09-22 01:16:59 +0200

    get latest ami id as poc

--- src/main/python/cfn_sphere/aws/ec2.py
+++ src/main/python/cfn_sphere/aws/ec2.py
@@ -5,13 +5,22 @@ class Ec2(object):
     def __init__(self, region="eu-west-1"):
         self.conn = connect_to_region(region)
 
-    def get_latest_taupage_image(self):
-        filters = [{'Name': 'name', 'Values': ['*Taupage-AMI-*']},
-                   {'Name': 'is-public', 'Values': ['false']},
-                   {'Name': 'state', 'Values': ['available']},
-                   {'Name': 'root-device-type', 'Values': ['ebs']}]
-        print self.conn.get_all_images(executable_by="self", filters=filters)
+    def get_taupage_images(self):
+        filters = {'name': ['Taupage-AMI-*'],
+                   'is-public': ['false'],
+                   'state': ['available'],
+                   'root-device-type': ['ebs']
+                   }
 
+        return self.conn.get_all_images(executable_by=["self"], filters=filters)
 
+    def get_latest_taupage_image_id(self):
+        images = {image.creationDate: image.id for image in self.get_taupage_images()}
 
-foo = Ec2().get_latest_taupage_image()
+        creation_dates = images.keys()
+        creation_dates.sort()
+        return images[creation_dates[0]]
+
+# creationDate, name, id, ownerId
+
+print Ec2().get_latest_taupage_image_id()
