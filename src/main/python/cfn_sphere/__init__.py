a2694bd22df2f3fd691b35c02164e981070b36e2
nmohaupt <niels.mohaupt@immobilienscout24.de>
2016-01-12 08:43:12 +0100

    feature(tag-support): Create CloudFormationStack with tags

--- src/main/python/cfn_sphere/__init__.py
+++ src/main/python/cfn_sphere/__init__.py
@@ -32,12 +32,13 @@ class StackActionHandler(object):
 
             template_url = stack_config.template_url
             working_dir = stack_config.working_dir
+            tags = stack_config.tags
 
             raw_template = FileLoader.get_file_from_url(template_url, working_dir)
             template = CloudFormationTemplateTransformer.transform_template(raw_template)
 
             parameters = self.parameter_resolver.resolve_parameter_values(stack_config.parameters, stack_name)
-            stack = CloudFormationStack(template, parameters, stack_name, self.region, stack_config.timeout)
+            stack = CloudFormationStack(template=template, parameters=parameters, tags=tags, name=stack_name, region=self.region, timeout=stack_config.timeout)
 
             if stack_name in existing_stacks:
 
