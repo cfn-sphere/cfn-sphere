359e0ee6a42719f34c7c7d4b678454bee6678e6a
nmohaupt <niels.mohaupt@immobilienscout24.de>
2016-01-18 15:55:14 +0100

    feature(global-tags): Add global tags to configuration

--- src/unittest/python/stack_configuration/stack_configuration_tests.py
+++ src/unittest/python/stack_configuration/stack_configuration_tests.py
@@ -5,12 +5,13 @@ from cfn_sphere.stack_configuration import Config, StackConfig, NoConfigExceptio
 
 class ConfigTests(unittest2.TestCase):
     def test_properties_parsing(self):
-        config = Config(config_dict={'region': 'eu-west-1', 'stacks': {'any-stack': {'template-url': 'foo.json', 'tags': {'any-tag': 'any-tag-value'}, 'parameters': {'any-parameter': 'any-value'}}}})
+        config = Config(config_dict={'region': 'eu-west-1', 'tags': {'global-tag': 'global-tag-value'}, 'stacks': {'any-stack': {'template-url': 'foo.json', 'tags': {'any-tag': 'any-tag-value'}, 'parameters': {'any-parameter': 'any-value'}}}})
         self.assertEqual('eu-west-1', config.region)
         self.assertEqual(1, len(config.stacks.keys()))
         self.assertTrue(isinstance(config.stacks['any-stack'], StackConfig))
         self.assertEqual('foo.json', config.stacks['any-stack'].template_url)
         self.assertEqual({'any-tag': 'any-tag-value'}, config.stacks['any-stack'].tags)
+        self.assertEqual({'global-tag': 'global-tag-value'}, config.tags)
         self.assertEqual({'any-parameter': 'any-value'}, config.stacks['any-stack'].parameters)
 
     def test_raises_exception_if_no_region_key(self):
