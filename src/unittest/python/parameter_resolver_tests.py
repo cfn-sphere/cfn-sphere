2a906b1e4ed2a9d836cea470eb22e5f99b18c884
Tobias Höynck <tobias.hoeynck@immobilienscout24.de>
2016-01-22 15:29:49 +0100

    CLOUD-333: PEP8 happiness

--- src/unittest/python/parameter_resolver_tests.py
+++ src/unittest/python/parameter_resolver_tests.py
@@ -7,12 +7,12 @@ from cfn_sphere.resolver.parameter_resolver import ParameterResolver
 class ParameterResolverTests(unittest2.TestCase):
 
     def test_convert_list_to_string_returns_valid_string(self):
-        list = ['a', 'b', 'c']
-        self.assertEqual("a,b,c", ParameterResolver.convert_list_to_string(list))
+        input_list = ['a', 'b', 'c']
+        self.assertEqual("a,b,c", ParameterResolver.convert_list_to_string(input_list))
 
     def test_convert_list_to_string_returns_valid_string_if_list_contains_int(self):
-        list = ['a', 6, 'c']
-        self.assertEqual("a,6,c", ParameterResolver.convert_list_to_string(list))
+        input_list = ['a', 6, 'c']
+        self.assertEqual("a,6,c", ParameterResolver.convert_list_to_string(input_list))
 
     def test_convert_list_to_string_returns_empty_list_on_empty_list(self):
         self.assertEqual("", ParameterResolver.convert_list_to_string([]))
