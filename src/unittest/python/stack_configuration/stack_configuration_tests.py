ce2a86d330162d86190a1629a05b7be56ae6c3d5
Tobias Höynck <tobias.hoeynck@immobilienscout24.de>
2016-01-25 13:58:11 +0100

    CLOUD-333: merged former config_test.py

--- src/unittest/python/stack_configuration/stack_configuration_tests.py
+++ src/unittest/python/stack_configuration/stack_configuration_tests.py
@@ -1,11 +1,15 @@
 import unittest2
 
 from cfn_sphere.stack_configuration import Config, StackConfig, NoConfigException
+from cfn_sphere.exceptions import BadConfigException
 
 
 class ConfigTests(unittest2.TestCase):
     def test_properties_parsing(self):
-        config = Config(config_dict={'region': 'eu-west-1', 'tags': {'global-tag': 'global-tag-value'}, 'stacks': {'any-stack': {'template-url': 'foo.json', 'tags': {'any-tag': 'any-tag-value'}, 'parameters': {'any-parameter': 'any-value'}}}})
+        config = Config(
+            config_dict={'region': 'eu-west-1', 'tags': {'global-tag': 'global-tag-value'},
+                         'stacks': {'any-stack': {'template-url': 'foo.json', 'tags': {'any-tag': 'any-tag-value'},
+                                                  'parameters': {'any-parameter': 'any-value'}}}})
         self.assertEqual('eu-west-1', config.region)
         self.assertEqual(1, len(config.stacks.keys()))
         self.assertTrue(isinstance(config.stacks['any-stack'], StackConfig))
@@ -21,3 +25,24 @@ class ConfigTests(unittest2.TestCase):
     def test_raises_exception_if_no_stacks_key(self):
         with self.assertRaises(NoConfigException):
             Config(config_dict={'region': 'eu-west-1'})
+
+    def test_properties_parsing_cli_params(self):
+        config = Config(cli_params="p1=v1,p2=v2",
+                        config_dict={'region': 'eu-west-1', 'stacks': {'foo': {'template-url': 'foo.json'}}})
+        self.assertTrue('p1' in config.cli_params)
+        self.assertTrue('p2' in config.cli_params)
+        self.assertTrue('v1' in config.cli_params.values())
+        self.assertTrue('v2' in config.cli_params.values())
+
+    def test_properties_parsing_invalid_cli_params(self):
+        with self.assertRaises(BadConfigException):
+            Config(cli_params="foobar",
+                   config_dict={'region': 'eu-west-1', 'stacks': {'foo': {'template-url': 'foo.json'}}})
+
+    def test_properties_parsing_just_config_file_and_cli_params(self):
+        with self.assertRaises(NoConfigException):
+            Config(cli_params="p1=v1,p2=v2", config_dict={'region': 'eu-west-1'})
+
+    def test_properties_parsing_just_cli_params(self):
+        with self.assertRaises(NoConfigException):
+            Config(cli_params="p1=v1,p2=v2")
