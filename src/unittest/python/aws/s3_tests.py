55e6503204fd48722e26033ba4e29a1da323812c
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2016-03-31 01:06:39 +0200

    refactor s3 tests

--- src/unittest/python/aws/s3_tests.py
+++ src/unittest/python/aws/s3_tests.py
@@ -1,9 +1,13 @@
+from botocore.response import StreamingBody
 import unittest2
+
 
 try:
     from unittest2 import TestCase
+    from mock import Mock, patch
 except ImportError:
     from unittest import TestCase
+    from unittest.mock import Mock, patch
 
 from cfn_sphere.aws.s3 import S3
 
@@ -14,3 +18,14 @@ class S3Tests(unittest2.TestCase):
         self.assertEqual('s3', protocol)
         self.assertEqual('my-bucket', bucket_name)
         self.assertEqual('my/key/file.json', key_name)
+
+    @patch('cfn_sphere.aws.s3.boto3.resource')
+    def test_get_contents_from_url_returns_string_content(self, resource_mock):
+        body_mock = Mock(spec=StreamingBody)
+        body_mock.read.return_value = b'Foo'
+
+        resource_mock.return_value.Object.return_value.get.return_value = {"Body": body_mock}
+
+        result = S3().get_contents_from_url('s3://my-bucket/my/key/file.json')
+        self.assertEqual("Foo", result)
+
