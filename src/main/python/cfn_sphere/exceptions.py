b46dfa71c644cc06e8bf1f78e5773f9258a974ec
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2016-03-30 17:52:43 +0200

    add exception descriptions

--- src/main/python/cfn_sphere/exceptions.py
+++ src/main/python/cfn_sphere/exceptions.py
@@ -1,32 +1,35 @@
 class CfnSphereException(Exception):
+    """ The base exception for Cfn-Sphere """
     pass
 
 
 class CfnStackActionFailedException(CfnSphereException):
+    """ Raised if a stack modification fails """
     pass
 
 
 class TemplateErrorException(CfnSphereException):
+    """ Raised on invalid templates """
     pass
 
 
 class NoConfigException(CfnSphereException):
-    pass
-
-
-class BadConfigException(CfnSphereException):
+    """ Raised if no stack configuration could be found """
     pass
 
 
 class CyclicDependencyException(CfnSphereException):
+    """ Raised if there are cyclic dependencies between referenced stacks """
     pass
 
 
 class InvalidDependencyGraphException(CfnSphereException):
+    """ Raised if Cfn-Sphere is not able to create a dependency graph for stacks """
     pass
 
 
 class InvalidEncryptedValueException(CfnSphereException):
+    """ Raised if Cfn-Sphere is unable to decrypt a KMS encrypted string """
     pass
 
 
