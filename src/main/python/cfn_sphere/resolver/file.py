6efd086b94f90177ac8d406c73adff435eee9741
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2016-04-18 13:12:56 +0200

    import exception

--- src/main/python/cfn_sphere/resolver/file.py
+++ src/main/python/cfn_sphere/resolver/file.py
@@ -1,9 +1,11 @@
+from cfn_sphere.exceptions import CfnSphereException
+
 
 class FileResolver(object):
-
-    def read(self, path):
+    @staticmethod
+    def read(path):
         try:
             with open(path, 'r') as f:
                 return f.read()
         except IOError as e:
-            raise CfnSphereException("Cannot read file " + path, e) 
+            raise CfnSphereException("Cannot read file " + path, e)
