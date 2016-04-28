6006443c24a76250a531172b6d783c386e7fccef
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2016-04-28 15:09:18 +0200

    add some tests for new file loader

--- src/main/python/cfn_sphere/aws/s3.py
+++ src/main/python/cfn_sphere/aws/s3.py
@@ -3,6 +3,7 @@ from boto3.exceptions import Boto3Error
 from botocore.exceptions import BotoCoreError, ClientError
 from six.moves.urllib.parse import urlparse
 
+from cfn_sphere.exceptions import CfnSphereBotoError
 from cfn_sphere.util import with_boto_retry
 
 
@@ -18,7 +19,6 @@ class S3(object):
         key = url_components.path.strip('/')
         return protocol, bucket_name, key
 
-    # TODO: rething boto exception handling and retry
     @with_boto_retry()
     def get_contents_from_url(self, url):
         try:
@@ -26,4 +26,4 @@ class S3(object):
             s3_object = self.s3.Object(bucket_name, key_name)
             return s3_object.get(ResponseContentEncoding='utf-8')["Body"].read().decode('utf-8')
         except (Boto3Error, BotoCoreError, ClientError) as e:
-            raise Exception(e)
+            raise CfnSphereBotoError(e)
