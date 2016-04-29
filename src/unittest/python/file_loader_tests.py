9288224314db5ec7de003e89a0541b62f8d5c48b
Marco Hoyer <marco.hoyer@immobilienscout24.de>
2016-04-29 20:59:49 +0200

    merge tests

--- src/unittest/python/file_loader_tests.py
+++ src/unittest/python/file_loader_tests.py
@@ -78,35 +78,10 @@ class FileLoaderTests(TestCase):
         with self.assertRaises(CfnSphereException):
             FileLoader.get_yaml_or_json_file('foo.yml', 'baa')
 
-    @patch("cfn_sphere.file_loader.yaml")
-    @patch("cfn_sphere.file_loader.open")
-    def test_fs_get_file_raises_exception_on_json_error(self, _, yaml_mock):
-        yaml_mock.load.side_effect = ValueError()
-
-        with self.assertRaises(TemplateErrorException):
-            FileLoader._fs_get_file('foo.json', 'baa')
-
-    def test_fs_get_file_raises_exception_on_unknown_filetype(self):
-        with self.assertRaises(TemplateErrorException):
-            FileLoader._fs_get_file('bla.blub', 'baa')
-
-    @patch("cfn_sphere.file_loader.FileLoader._fs_get_file")
-    def test_load_template_calls_fs_get_file_for_fs_url(self, mock):
-        url = "/tmp/template.json"
-
-        loader = FileLoader
-        loader.get_file_from_url(url, None)
-
-        mock.assert_called_with(url, None)
-
-    @patch("cfn_sphere.file_loader.FileLoader._s3_get_file")
-    def test_load_template_calls_s3_get_file_for_s3_url(self, mock):
-        url = "s3://my-bucket.amazon.com/foo.json"
-
-        loader = FileLoader
-        loader.get_file_from_url(url, None)
-
-        mock.assert_called_with(url)
+    @patch("cfn_sphere.file_loader.json")
+    @patch("cfn_sphere.file_loader.FileLoader.get_file")
+    def test_get_yaml_or_json_file_raises_exception_on_json_error(self, _, json_mock):
+        json_mock.loads.side_effect = ValueError()
 
         with self.assertRaises(CfnSphereException):
             FileLoader.get_yaml_or_json_file('foo.json', 'baa')
@@ -121,49 +96,31 @@ class FileLoaderTests(TestCase):
         FileLoader.get_file("S3://foo/foo.yml", None)
         s3_get_file_mock.assert_called_with("S3://foo/foo.yml")
 
-    def test_load_template_raises_exception_on_unknown_protocol(self):
-        url = "xxx://foo.json"
+    @patch("cfn_sphere.file_loader.FileLoader._fs_get_file")
+    def test_get_file_calls_correct_handler_for_fs_path(self, fs_get_file_mock):
+        FileLoader.get_file("foo/foo.yml", "/home/user")
+        fs_get_file_mock.assert_called_with("foo/foo.yml", "/home/user")
 
-        loader = FileLoader
+    @patch("cfn_sphere.file_loader.codecs.open")
+    def test_fs_get_file_constructs_absolute_path_if_url_is_relative_and_workingdir_is_set(self, open_mock):
+        FileLoader._fs_get_file("foo/foo.yml", "/home/user")
+        open_mock.assert_called_once_with("/home/user/foo/foo.yml", 'r', encoding='utf-8')
 
-        with self.assertRaises(TemplateErrorException):
-            loader.get_file_from_url(url, None)
+    @patch("cfn_sphere.file_loader.codecs.open")
+    def test_fs_get_file_absolute_url_without_modification(self, open_mock):
+        FileLoader._fs_get_file("/foo/foo.yml", "/home/user")
+        open_mock.assert_called_once_with("/foo/foo.yml", 'r', encoding='utf-8')
 
-    @patch('cfn_sphere.file_loader.yaml.load')
-    @patch('cfn_sphere.file_loader.json.loads')
-    @patch('cfn_sphere.file_loader.S3')
-    def test_s3_get_file_returns_parses_json_for_json_suffix(self, s3_mock, json_mock, yaml_mock):
-        s3_mock.return_value.get_contents_from_url.return_value = "{}"
+    @patch("cfn_sphere.file_loader.codecs.open")
+    def test_fs_get_file_raises_exception_on_any_error(self, open_mock):
+        open_mock.side_effect = IOError
+        with self.assertRaises(CfnSphereException):
+            FileLoader._fs_get_file("/foo/foo.yml", None)
 
-        FileLoader._s3_get_file('s3://foo/baa.json')
-        json_mock.assert_called_once_with("{}")
-        yaml_mock.assert_not_called()
+    @patch("cfn_sphere.file_loader.S3")
+    def test_s3_get_file_raises_exception_on_error(self, s3_mock):
+        s3_mock.return_value.get_contents_from_url.side_effect = CfnSphereBotoError
 
-    @patch('cfn_sphere.file_loader.yaml.load')
-    @patch('cfn_sphere.file_loader.json.loads')
-    @patch('cfn_sphere.file_loader.S3')
-    def test_s3_get_file_parses_yaml_for_yaml_suffix(self, s3_mock, json_mock, yaml_mock):
-        s3_mock.return_value.get_contents_from_url.return_value = "{}"
+        with self.assertRaises(CfnSphereException):
+            FileLoader._s3_get_file("s3://foo/foo.yml")
 
-        FileLoader._s3_get_file('s3://foo/baa.yaml')
-        json_mock.assert_not_called()
-        yaml_mock.assert_called_once_with("{}")
-
-    @patch('cfn_sphere.file_loader.yaml.load')
-    @patch('cfn_sphere.file_loader.json.loads')
-    @patch('cfn_sphere.file_loader.S3')
-    def test_s3_get_file_parses_yaml_for_yml_suffix(self, s3_mock, json_mock, yaml_mock):
-        s3_mock.return_value.get_contents_from_url.return_value = "{}"
-
-        FileLoader._s3_get_file('s3://foo/baa.yml')
-        json_mock.assert_not_called()
-        yaml_mock.assert_called_once_with("{}")
-
-    @patch('cfn_sphere.file_loader.yaml.load')
-    @patch('cfn_sphere.file_loader.json.loads')
-    @patch('cfn_sphere.file_loader.S3')
-    def test_s3_get_file__raises_exception_for_unknown_suffix(self, s3_mock, json_mock, yaml_mock):
-        s3_mock.return_value.get_contents_from_url.return_value = "{}"
-
-        with self.assertRaises(TemplateErrorException):
-            FileLoader._s3_get_file('s3://foo/baa.foo')
