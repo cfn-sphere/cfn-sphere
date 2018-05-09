77d5127adbef1b12735218a8e07bdcbd37b29c27
marco-hoyer <marco.hoyer@scout24.com>
2018-05-09 09:20:21 +0200

    make integration tests run for python 2.7 and 3.6

--- bin/run_integration_tests.sh
+++ bin/run_integration_tests.sh
@@ -2,3 +2,4 @@
 set -x
 
 docker run -it -v "$(pwd):/cfn" -v "$HOME/.aws:/root/.aws" --entrypoint /cfn/bin/execute_integration_tests.sh python:2.7
+docker run -it -v "$(pwd):/cfn" -v "$HOME/.aws:/root/.aws" --entrypoint /cfn/bin/execute_integration_tests.sh python:3.6
